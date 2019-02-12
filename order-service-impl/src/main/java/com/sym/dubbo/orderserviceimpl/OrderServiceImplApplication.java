a4fd076aacb117ef916934e69a3fa398ed9e5597
suyiming3333 <suyiming3333@gmail.com>
2019-02-12 22:42:19 +0800

    init

---
+++ order-service-impl/src/main/java/com/sym/dubbo/orderserviceimpl/OrderServiceImplApplication.java
@@ -0,0 +1,14 @@
+package com.sym.dubbo.orderserviceimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderServiceImplApplication.class, args);
+    }
+
+}
+
---
+++ order-service-impl/src/test/java/com/sym/dubbo/orderserviceimpl/OrderServiceImplApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sym.dubbo.orderserviceimpl;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderServiceImplApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
